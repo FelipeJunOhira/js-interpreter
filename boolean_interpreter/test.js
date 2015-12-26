76364956b8cab3f51423eecfdca69e280cad3c0a
Jun <felipe.jun.ohira@gmail.com>
2015-12-26 21:00:41 -0200

    Refactoring tests and adding FalseExpression test

--- boolean_interpreter/test.js
+++ boolean_interpreter/test.js
@@ -2,20 +2,39 @@ var expect = require('chai').expect;
 
 var Expression = require('./expression/expression');
 var TrueExpression = require('./expression/true_expression');
+var FalseExpression = require('./expression/false_expression');
 
-describe('TrueExpression', function() {
-  it('can be instantiated', function() {
-    expect(new TrueExpression()).to.be.instanceOf(TrueExpression);
+describe('Expressions', function() {
+
+  var behaveAsExpression = function(ExpressionLike) {
+    it('can be instantiated', function() {
+      expect(new ExpressionLike()).to.be.instanceOf(ExpressionLike);
+    });
+
+    it('inherit from Expression', function() {
+      expect(new ExpressionLike()).to.be.instanceOf(Expression);
+    });
+  }
+
+  describe('TrueExpression', function() {
+    behaveAsExpression(TrueExpression);
+
+    describe('.evaluate', function() {
+      it('returns true', function() {
+        var expression = new TrueExpression();
+        expect(expression.evaluate()).to.be.equal(true);
+      });
+    });
   });
 
-  it('inherit from Expression', function() {
-    expect(new TrueExpression()).to.be.instanceOf(Expression);
-  });
+  describe('FalseExpression', function() {
+    behaveAsExpression(FalseExpression);
 
-  describe('.evaluate', function() {
-    it('returns true', function() {
-      var expression = new TrueExpression();
-      expect(expression.evaluate()).to.be.equal(true);
+    describe('.evaluate', function() {
+      it('returns false', function() {
+        var expression = new FalseExpression();
+        expect(expression.evaluate()).to.be.equal(false);
+      });
     });
   });
 });
