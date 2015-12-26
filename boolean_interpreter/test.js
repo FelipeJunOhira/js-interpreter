822e14823ebae2bb1fcede7c5be742f472d54f1c
Jun <felipe.jun.ohira@gmail.com>
2015-12-26 20:50:19 -0200

    Writting test for TrueExpression

--- boolean_interpreter/test.js
+++ boolean_interpreter/test.js
@@ -1,1 +1,9 @@
 var expect = require('chai').expect;
+
+var TrueExpression = require('./expression/true_expression');
+
+describe('TrueExpression', function() {
+  it('can be instantiated', function() {
+    expect(new TrueExpression()).to.be.instanceOf(TrueExpression);
+  });
+});
