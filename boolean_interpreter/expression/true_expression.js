74053cc659dd46fcbf9afffe9c9d5a2f1095e83a
Jun <felipe.jun.ohira@gmail.com>
2015-12-26 20:52:50 -0200

    Making TrueExpression inherit from Expression

--- boolean_interpreter/expression/true_expression.js
+++ boolean_interpreter/expression/true_expression.js
@@ -4,6 +4,9 @@ module.exports = (function() {
 
   function TrueExpression() {};
 
+  TrueExpression.prototype = Object.create(Expression.prototype);
+  TrueExpression.prototype.constructor = TrueExpression;
+
   TrueExpression.prototype.evaluate = function() {
     return true;
   };
--- boolean_interpreter/test.js
+++ boolean_interpreter/test.js
@@ -1,9 +1,14 @@
 var expect = require('chai').expect;
 
+var Expression = require('./expression/expression');
 var TrueExpression = require('./expression/true_expression');
 
 describe('TrueExpression', function() {
   it('can be instantiated', function() {
     expect(new TrueExpression()).to.be.instanceOf(TrueExpression);
+  });
+
+  it('inherit from Expression', function() {
+    expect(new TrueExpression()).to.be.instanceOf(Expression);
   });
 });
